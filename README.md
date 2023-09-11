1038ce44aa164df6db673cf490c3c4050a68d802
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2023-09-11 17:19:11 -0500

    Initial commit
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# This is a WIP C++ template repository
+
+### How to compile
+1. `cmake -B build`
+2. `make -C build -j $(nproc)`
+
+### How to run
+- `./build/main`
+
+### How to test
+- `./build/functions/functions_test`
+or
+- `cd build/functions`
+- `ctest`
---
+++ functions/functions.cpp
@@ -0,0 +1,49 @@
+/*
+ * This file showcases different ways of accessing a variable
+ */
+
+#include <iostream>
+#include <array>
+#include <unistd.h>
+
+#include "functions.hpp"
+#include "rocm_smi/rocm_smi.h"
+using namespace std;
+
+/*
+ * Declaration of a reference without instantiation is not allowed
+ * The line below will not compile:
+ * int & my_reference;
+ */
+
+void pass_by_value(int arg) {
+    printf("arg: %p:%d\n", (void *) &arg, arg);
+    arg++;
+    printf("arg: %p:%d\n", (void *) &arg, arg);
+}
+
+void pass_by_reference(int & arg) {
+    printf("arg: %p:%d\n", (void *) &arg, arg);
+    arg++;
+    printf("arg: %p:%d\n", (void *) &arg, arg);
+}
+
+void pass_by_pointer(int * arg) {
+    printf("arg: %p:%d\n", (void *) arg, *arg);
+    (*arg)++;
+    printf("arg: %p:%d\n", (void *) arg, *arg);
+}
+
+void run_functions() {
+    int my_int = 0;
+    printf("initial\n");
+    printf("my_int = %d\n\n", my_int);
+
+    printf("pass_by_value\n");
+    pass_by_value(my_int);
+    printf("my_int = %d\n\n", my_int);
+
+    printf("pass_by_reference\n");
+    pass_by_reference(my_int);
+    printf("my_int = %d\n\n", my_int);
+}
